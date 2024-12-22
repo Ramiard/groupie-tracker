eb6059bed893376b2a7d4a7e17e1c40b86c4da8a
Ramiard <renaud.amiard@ynov.com>
2024-12-22 23:39:17 +0100

    feat(Structs): Creation of needed structs
    Create few structs :
    - Relation (contain Id and the Dates and Locations of a given music group)
    - GroupInfos (containing all infos that provide the API of a given music group)
    - GroupList (containing a list of GroupInfos)

---
+++ Internal/Api/Structures.go
@@ -0,0 +1,24 @@
+package Api
+
+type GroupList struct {
+	List []GroupInfos
+}
+
+type GroupInfos struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	RelationsUrl string   `json:"relations"`
+	Relations    Relation
+}
+
+type Relation struct {
+	Id             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+//var Groups GroupList
+//var Group GroupInfos
