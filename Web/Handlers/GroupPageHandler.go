2b0a77a15bc1cac00a913340d04b7f9a692fe370
Ramiard <renaud.amiard@ynov.com>
2024-12-27 16:43:12 +0100

    feat(GroupPage): Add the Handler and the WebPage
    GroupPageHandler.go will firstly check if you send him a 'post' request, if not it will redirect you to the homePage.
    Next it will check if you send the 'id' in the right format, if it's not empty and if the 'id' exist. If not it will display an error.
    
    GroupPage.gohtml : will display all the information of the given group

---
+++ Web/Handlers/GroupPageHandler.go
@@ -0,0 +1,49 @@
+package Handlers
+
+import (
+	"fmt"
+	"groupie-tracker/Internal/Api"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func GroupPageHandler(w http.ResponseWriter, r *http.Request) {
+	// Load the template
+	tmpl := template.Must(template.ParseFiles("Web/Templates/GroupPage.gohtml"))
+
+	// Get the form value
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Error while analysing the form", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the user send a POST request containing an ID
+	if r.Method != http.MethodPost {
+		fmt.Println("LOG: No POST used redirecting to /HomePage")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	fmt.Print("LOG: Given ID : '", r.FormValue("id"), "' \n")
+
+	// Get the group infos
+	groupInfos := Api.GetGroupInfos(r.FormValue("id"))
+
+	// Convert the form value 'string' to an 'int'
+	_, err = strconv.Atoi(r.FormValue("id"))
+	// Check if the Id is empty or if it's not a number
+	if r.FormValue("id") == "" || groupInfos.Id == 0 || err != nil {
+		fmt.Println("LOG: Group not found or Id is in a wrong format")
+		http.Error(w, "Group not found.\nOr the given Id is in a wrong format", http.StatusBadRequest)
+		return
+	}
+
+	// Run the template
+	err = tmpl.ExecuteTemplate(w, "GroupPage", groupInfos)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while loading the template : %v", err), http.StatusInternalServerError)
+	}
+
+}
