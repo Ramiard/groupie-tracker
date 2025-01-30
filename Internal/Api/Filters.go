2a3a4a23c50d791593d492d32e33d5d2ef7ec07a
Ramiard <renaud.amiard@ynov.com>
2025-01-30 14:20:08 +0100

    feat(Filters Backend): Add of QtyOfMembers filter
    HomePage.gohtml :
    Add the QtyOfMembers form
    
    Filters.go :
    Add the filter, make a list that contain only groups who have the selected minimum of members and the maximum selected.
    
    HomePageHandler.go :
    Maked few change in the logic, the 'if' condition that was checking if the filter is not empty was not working because range form never send empty data.
    After that I adapted the code so it can handle the 2 actual filter, when the first is done, the second one will filter the already filtered data.

--- Internal/Api/Filters.go
+++ Internal/Api/Filters.go
@@ -10,3 +10,14 @@ func FilterGroupsByCreationDate(minCreationDate int, maxCreationDate int, groups
 	}
 	return filteredGroups
 }
+
+func FilterGroupsByQtyOfMembers(minQtyOfMembers int, maxQtyOfMembers int, groups []GroupInfos) []GroupInfos {
+	var filteredGroups []GroupInfos
+
+	for _, group := range groups {
+		if group.QtyOfMembers >= minQtyOfMembers && group.QtyOfMembers <= maxQtyOfMembers {
+			filteredGroups = append(filteredGroups, group)
+		}
+	}
+	return filteredGroups
+}
--- Web/Handlers/HomePageHandler.go
+++ Web/Handlers/HomePageHandler.go
@@ -32,29 +32,53 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if r.FormValue("filterBy-CreationDate-min") != "" && r.FormValue("filterBy-CreationDate-max") != "" {
+		// Checking the 'CreationDate' filter
+		fmt.Println("FILTER LOG : minCreationDate = [", r.FormValue("filterBy-CreationDate-min"), "] AND maxCreationDate = [", r.FormValue("filterBy-CreationDate-max"), "]")
 
-			// Check if the user sent an integer
-			minCreationDate, err := strconv.Atoi(r.FormValue("filterBy-CreationDate-min"))
-			if err != nil {
-				http.Error(w, "Error, the value of 'minCreationDate' that you sent is not an integer", http.StatusBadRequest)
-				return
-			}
-			maxCreationDate, err := strconv.Atoi(r.FormValue("filterBy-CreationDate-max"))
-			if err != nil {
-				http.Error(w, "Error, the value of 'maxCreationDate' that you sent is not an integer", http.StatusBadRequest)
-				return
-			}
-
-			// Check if the user sent a valid range
-			if minCreationDate > maxCreationDate {
-				http.Error(w, "Error, the minimum value is higher than the maximum value", http.StatusBadRequest)
-				return
-			}
-
-			// Apply the filter
-			GroupList = Api.FilterGroupsByCreationDate(minCreationDate, maxCreationDate, GroupList)
+		// Check if the user sent an integer
+		minCreationDate, err := strconv.Atoi(r.FormValue("filterBy-CreationDate-min"))
+		if err != nil {
+			http.Error(w, "Error, the value of 'minCreationDate' that you sent is not an integer", http.StatusBadRequest)
+			return
 		}
+		maxCreationDate, err := strconv.Atoi(r.FormValue("filterBy-CreationDate-max"))
+		if err != nil {
+			http.Error(w, "Error, the value of 'maxCreationDate' that you sent is not an integer", http.StatusBadRequest)
+			return
+		}
+
+		// Check if the user sent a valid range
+		if minCreationDate > maxCreationDate {
+			http.Error(w, "Error, the minimum value is higher than the maximum value", http.StatusBadRequest)
+			return
+		}
+
+		// Apply the 'CreationDate' filter
+		GroupList = Api.FilterGroupsByCreationDate(minCreationDate, maxCreationDate, GroupList)
+
+		// Checking the 'QtyOfMembers' filter
+		fmt.Println("FILTER LOG : minQtyOfMembers = [", r.FormValue("filterBy-NumberOfMembers-min"), "] AND maxQtyOfMembers = [", r.FormValue("filterBy-NumberOfMembers-max"), "]")
+
+		// Check if the user sent an integer
+		minQtyOfMembers, err := strconv.Atoi(r.FormValue("filterBy-NumberOfMembers-min"))
+		if err != nil {
+			http.Error(w, "Error, the value of 'minQtyOfMembers' that you sent is not an integer", http.StatusBadRequest)
+			return
+		}
+		maxQtyOfMembers, err := strconv.Atoi(r.FormValue("filterBy-NumberOfMembers-max"))
+		if err != nil {
+			http.Error(w, "Error, the value of 'maxQtyOfMembers' that you sent is not an integer", http.StatusBadRequest)
+			return
+		}
+
+		// Check if the user sent a valid range
+		if minQtyOfMembers > maxQtyOfMembers {
+			http.Error(w, "Error, the minimum value is higher than the maximum value", http.StatusBadRequest)
+			return
+		}
+
+		// Apply the filter
+		GroupList = Api.FilterGroupsByQtyOfMembers(minQtyOfMembers, maxQtyOfMembers, GroupList)
 
 		// Run the template with the filtered data
 		err = tmpl.ExecuteTemplate(w, "HomePage", GroupList)
