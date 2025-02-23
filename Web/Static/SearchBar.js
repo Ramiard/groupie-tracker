fe9266a4999db192f722d310fd5a03fc6dd5208a
Ramiard <renaud.amiard@ynov.com>
2025-02-23 17:37:57 +0100

    feat(SearchBar): Making the searchBar to make suggestions
    The SearchBar now makes suggestions.
    For example, if you enter 'Freddie' it will suggest 'Freddie Mercury | Member of Queen'
    
    HomePage.gohtml :
    - Added a datalist to put the suggestions in it
    
    SearchBar.js :
    - Maked a 'makeSuggestion' function that will append an 'option' markup to the datalist that contain the suggestion from what the user entered
    - Temporarily removed the 'number of member' suggestion because im not really sure that is relevant
    
    - Added a input length restriction before making suggestion to not flood the user with irrelevant suggestions
    REMAIN TO DO :
    - when the user click on the suggestion, all the text of the suggestion is in the user input making it impossible to search
    (for example, if the user click on 'Freddie Mercury | Member of Queen'. All the text will be search.
    I want it to enter only 'Freddie Mercury'
    
    SearchResults.gohtml :
    - Making it work with the predictive search

--- Web/Static/SearchBar.js
+++ Web/Static/SearchBar.js
@@ -4,18 +4,79 @@ window.addEventListener('load', () => {
     // Parse it
     const data = JSON.parse(jsonData.textContent);
 
+
+    // data.groups[0].members.forEach((member) => {
+    //     console.log(`${member}`);
+    // })
+
     // Get the search input
+    let searchInput = document.getElementById("searchBar");
     document.getElementById("searchBar").addEventListener('input', e => {
+
         // Get the user input
         const input = e.target.value.toLowerCase();
+        const suggestions = document.getElementById("suggestionList");
+        // Clear the suggestions
+        suggestions.innerHTML = "";
         console.log(`User input: ${input}`);
 
-        // Search it in the groups names
-        data.groups.forEach( (group)  => {
-            if (group.name.toLowerCase().includes(input)) {
-                console.log(`Group found: ${group.name}`);
-            }
-        });
+
+        if (input.length >= 2) {
+            // Search it in the groups names
+            data.groups.forEach((group) => {
+                // Check if the group 'name' contains the input
+                if (group.name.toLowerCase().includes(input)) {
+                    makeSuggestion(`Group found: ${group.name}`);
+                }
+                // Check if the group 'members' contains the input
+                group.members.forEach((member) => {
+                    if (member.toLowerCase().includes(input)) {
+                        makeSuggestion(`${member} | Member of ${group.name}`);
+                    }
+                });
+                // Check if the group 'number of members' contains the input
+                // if (group.qtyOfMembers.toString().includes(input)) {
+                //     makeSuggestion(`Group found by 'QTY of members': ${group.name} | Number of members: ${group.qtyOfMembers}`);
+                // }
+
+                // Check if the group 'creation date' contains the input
+                // If the input is to small we don't suggest the creation date (to avoid to flood the suggestion)
+                if (input.length > 3) {
+                    if (group.creationDate.toString().includes(input)) {
+                        makeSuggestion(`${group.name}'s creation date: ${group.creationDate}`);
+                    }
+                }
+                // Check if the group 'first album' date contains the input
+                // If the input is to small we don't suggest the first album date (to avoid to flood the suggestion)
+                if (input.length > 3) {
+                    if (group.firstAlbum.includes(input)) {
+                        makeSuggestion(`${group.name}'s first album: ${group.firstAlbum}`);
+                    }
+                }
+                // Check if the group 'locations' and 'dates' contains the input
+                for (const [key, value] of Object.entries(group.allRelations.datesLocations)) {
+                    if (key.toLowerCase().includes(input)) {
+                        makeSuggestion(`${group.name}'s concert location: ${key}`);
+                    }
+                    if (input.length > 3) {
+                        value.forEach((date) => {
+                            if (date.includes(input)) {
+                                makeSuggestion(`${group.name}'s ${key} concert date ${date}`);
+                            }
+                        });
+                    }
+                }
+
+            });
+        }
+
 
     });
+    function makeSuggestion(str) {
+        const suggestion = document.createElement("option");
+        suggestion.setAttribute("value", str);
+        suggestion.setAttribute("class", "suggestion")
+        suggestion.textContent = str;
+        document.getElementById("suggestionList").appendChild(suggestion);
+    }
 });
