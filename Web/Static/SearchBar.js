aa9e799ef0ecd66ab4663a7dbcfd9b43b9092a3d
Ramiard <renaud.amiard@ynov.com>
2025-02-24 16:02:50 +0100

    feat(SearchBar): Make the prediction work for Firefox
    Before the change the prediction wasn't working on Firefox but only on Chrome.
    It was because the 'dynamic' datalist markup don't work on Firefox.
    
    To fix that i had the change the functionning of the 'makeSuggestion' for it to make UL - LI markups instead of the datalist
    
    SearchResults.gohtml / HomePage.gohtml :
    - Change the id's to be more conventional
    - Delete the datalist
    
    HomePage.css :
    - Added all the CSS needed to have a stylized UL -LI
    
    HomePageHandler.go :
    - Delete the json function (it wasn't useful)
    
    SearchBar.js :
    - Recommented all the code to make it more readable
    - Make the 'makeSuggestion' function work with Firefox by make UL - LI instead of datalist

--- Web/Static/SearchBar.js
+++ Web/Static/SearchBar.js
@@ -1,67 +1,75 @@
 window.addEventListener('load', () => {
     // Get the JSON data
-    const jsonData = document.getElementById("json-data");
+    const jsonData = document.getElementById("json_data");
     // Parse it
     const data = JSON.parse(jsonData.textContent);
 
+    // Create the suggestions container
+    const suggestions =  document.createElement("ul");
+    suggestions.setAttribute("id","suggestions_container");
 
-    // data.groups[0].members.forEach((member) => {
-    //     console.log(`${member}`);
-    // })
+    // 'i' will be used to know when to put the suggestion_container on the html
+    let i = 0;
 
     // Get the search input
-    let searchInput = document.getElementById("searchBar");
-    document.getElementById("searchBar").addEventListener('input', e => {
-
+    document.getElementById("search_bar").addEventListener('input', e => {
         // Get the user input
         const input = e.target.value.toLowerCase();
-        const suggestions = document.getElementById("suggestionList");
-        // Clear the suggestions
+
+        i = input.length;
+        // Check the 'i' value to know when to put the suggestions container on the html
+        // if the user didn't write anything we don't need to show the suggestions
+        if (i === 1) {
+            e.target.insertAdjacentElement('afterend',suggestions);
+        } else if (i === 0) {
+            document.getElementById("suggestions_container").remove();
+        }
+
+        // Clear the suggestions at each input to know show the old suggestions
         suggestions.innerHTML = "";
         console.log(`User input: ${input}`);
 
 
-        if (input.length >= 2) {
-            // Search it in the groups names
+        if (input.length >= 1) {
+            // Parcours all the groups
             data.groups.forEach((group) => {
                 // Check if the group 'name' contains the input
                 if (group.name.toLowerCase().includes(input)) {
-                    makeSuggestion(`${group.name}`,'Group name');
+                    makeSuggestion(`${group.image}`,`${group.name}`,`/`);
                 }
+
                 // Check if the group 'members' contains the input
                 group.members.forEach((member) => {
                     if (member.toLowerCase().includes(input)) {
-                        makeSuggestion(`${member}`,`Member of ${group.name}`);
+                        makeSuggestion(`${group.image}`,`|${member}| Member of ${group.name}`,`/`);
                     }
                 });
-                // Check if the group 'number of members' contains the input
-                // if (group.qtyOfMembers.toString().includes(input)) {
-                //     makeSuggestion(`Group found by 'QTY of members': ${group.name} | Number of members: ${group.qtyOfMembers}`);
-                // }
 
                 // Check if the group 'creation date' contains the input
                 // If the input is to small we don't suggest the creation date (to avoid to flood the suggestion)
                 if (input.length > 3) {
                     if (group.creationDate.toString().includes(input)) {
-                        makeSuggestion(`${group.creationDate}`,`${group.name}'s creation date`);
+                        makeSuggestion(`${group.image}`,`|${group.creationDate}| ${group.name}'s creation date`,`/`);
                     }
                 }
+
                 // Check if the group 'first album' date contains the input
                 // If the input is to small we don't suggest the first album date (to avoid to flood the suggestion)
                 if (input.length > 3) {
                     if (group.firstAlbum.includes(input)) {
-                        makeSuggestion(`${group.firstAlbum}`,`${group.name}'s first album date`);
+                        makeSuggestion(`${group.image}`,`|${group.firstAlbum}| ${group.name}'s first album date`,`/`);
                     }
                 }
+
                 // Check if the group 'locations' and 'dates' contains the input
                 for (const [key, value] of Object.entries(group.allRelations.datesLocations)) {
                     if (key.toLowerCase().includes(input)) {
-                        makeSuggestion(`${key}`,`${group.name}'s concert location`);
+                        makeSuggestion(`${group.image}`,`|${key}| ${group.name}'s concert location`,`/`);
                     }
                     if (input.length > 3) {
                         value.forEach((date) => {
                             if (date.includes(input)) {
-                                makeSuggestion(`${date}`,`${group.name}'s,  ${key} concert date`);
+                                makeSuggestion(`${group.image}`,`|${date}| ${group.name}'s  ${key} concert date`,`/`);
                             }
                         });
                     }
@@ -72,11 +80,24 @@ window.addEventListener('load', () => {
 
 
     });
-    function makeSuggestion(value,text) {
-        const suggestion = document.createElement("option");
-        suggestion.setAttribute("value", value);
-        suggestion.setAttribute("class", "suggestion")
-        suggestion.textContent = text;
-        document.getElementById("suggestionList").appendChild(suggestion);
+    // 'makeSuggestion' will create a suggestion and append it to the suggestions container
+    function makeSuggestion(img,text,url) {
+        // Make the suggestion base and append it to the suggestions container
+        const suggestion = document.createElement("li");
+        suggestion.setAttribute("class","suggestion")
+        document.getElementById("suggestions_container").appendChild(suggestion);
+
+        // Make the suggestion image and append it to the suggestion
+        const suggestionImg = document.createElement("img");
+        suggestionImg.setAttribute("class","suggestion-image");
+        suggestionImg.setAttribute("src", img);
+        suggestion.appendChild(suggestionImg);
+
+        // Make the suggestion text and append it to the suggestion img
+        const suggestionText = document.createElement("a");
+        suggestionText.setAttribute("class","suggestion-text");
+        suggestionText.setAttribute("href",url);
+        suggestionText.textContent = text;
+        suggestionImg.insertAdjacentElement('afterend',suggestionText);
     }
 });
